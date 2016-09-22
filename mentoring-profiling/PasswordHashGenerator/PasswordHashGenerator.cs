8bebfa0d4f3ff5e12d36c5a3f2aac133eab84ed7
Dmitry Veresovich <Dmitry_Veresovich@epam.com>
2016-09-22 15:29:19 +0300

    Added hash password generator and attempted to optimize it

---
+++ mentoring-profiling/PasswordHashGenerator/PasswordHashGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PasswordHashGenerator
+{
+    public static class PasswordHashGenerator
+    {
+        //Original. Average ticks elapsed: ~200.000
+        //public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        //{
+        //    var iterate = 10000;
+        //    var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+        //    byte[] hash = pbkdf2.GetBytes(20);
+
+        //    byte[] hashBytes = new byte[36];
+        //    Array.Copy(salt, 0, hashBytes, 0, 16);
+        //    Array.Copy(hash, 0, hashBytes, 16, 20);
+
+        //    var passwordHash = Convert.ToBase64String(hashBytes);
+
+        //    return passwordHash;
+        //}
+
+        // Optimized. Average ticks elapsed: ~192.000
+        public static string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        {
+            var iterate = 10000;
+
+            var hash = new Rfc2898DeriveBytes(passwordText, salt, iterate).GetBytes(20);
+
+            var hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
---
+++ mentoring-profiling/PasswordHashGenerator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace PasswordHashGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProfileUsingStopWatch();
+
+            //ProfileUsingPerformanceAnalyzer();
+
+            //RegularRun();
+        }
+
+        private static void ProfileUsingPerformanceAnalyzer()
+        {
+            var password = "Dmitry12345";
+            byte[] salt = new byte[16];
+            var watch = new Stopwatch();
+            watch.Start();
+            var hash = PasswordHashGenerator.GeneratePasswordHashUsingSalt(password, salt);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedTicks);
+        }
+
+        private static void RegularRun()
+        {
+            do
+            {
+                Console.Write("Enter password: ");
+                var password = Console.ReadLine();
+
+                byte[] salt = new byte[16];
+                var hash = PasswordHashGenerator.GeneratePasswordHashUsingSalt(password, salt);
+                Console.WriteLine($"Hash: {hash}");
+                Console.WriteLine("Press any key to continue, Escape to exit.");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+
+        private static void ProfileUsingStopWatch()
+        {
+            int iterations = 10;
+
+            long minTime = long.MaxValue;
+            for (int i = 0; i < iterations; i++)
+            {
+                var password = "Dmitry12345";
+                byte[] salt = new byte[16];
+
+                var watch = new Stopwatch();
+                watch.Start();
+                var hash = PasswordHashGenerator.GeneratePasswordHashUsingSalt(password, salt);
+                watch.Stop();
+                if (minTime > watch.ElapsedTicks) minTime = watch.ElapsedTicks;
+            }
+
+            Console.WriteLine(minTime);
+        }
+    }
+}
